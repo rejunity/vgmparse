85d310fa942d5d53fe1a6fde434a7b00323992a4
Renaldas Zioma <renaldas.zioma@gmail.com>
2023-10-24 12:46:26 +0200

    Update url and author info in setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='vgmparse',
     version='2.0.2',
     description='VGM (Video Game Music) file parser',
-    url='https://github.com/cdodd/vgmparse',
-    author='Craig Dodd',
-    author_email='craig@dodd.io',
+    url='https://github.com/rejunity/vgmparse',
+    author='Craig Dodd', 'Renaldas Zioma'
+    author_email='craig@dodd.io', 'rzioma@acm.org'
     license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
